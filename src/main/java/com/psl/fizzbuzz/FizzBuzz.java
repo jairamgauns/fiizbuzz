456252c9dc37341373f2b2ca0fbad3117921e32d
jairam_gauns <jairam_gauns@persistent.com>
2021-02-07 17:21:44 +0530

    Added a function to print numbers from 1 to 100. Added a failing test case to test print Fizz for case divide by 3

--- src/main/java/com/psl/fizzbuzz/FizzBuzz.java
+++ src/main/java/com/psl/fizzbuzz/FizzBuzz.java
@@ -1,4 +1,12 @@
 package com.psl.fizzbuzz;
 
 public class FizzBuzz {
+
+    public String printNumbers() {
+        String number = "";
+        for (int i=1; i<=100; i++) {
+            number = number + " " + i;
+        }
+        return number;
+    }
 }
--- src/test/java/com/psl/fizzbuzz/FizzBuzzTest.java
+++ src/test/java/com/psl/fizzbuzz/FizzBuzzTest.java
@@ -1,4 +1,24 @@
 package com.psl.fizzbuzz;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
 public class FizzBuzzTest {
+
+    FizzBuzz fizzBuzz;
+
+    @BeforeEach
+    public void init () {
+        fizzBuzz = new FizzBuzz();
+        System.out.println("Initialized Class");
+    }
+
+    @Test
+    public void testCanReturnFizzIFDivideByThree () {
+         String value = fizzBuzz.printNumbers();
+        Assertions.assertEquals("Fizz", value);
+    }
 }

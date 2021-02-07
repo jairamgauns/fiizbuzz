d30eddf879bcc1dceaa9025d55f8ed3587dfa237
jairam_gauns <jairam_gauns@persistent.com>
2021-02-07 17:55:09 +0530

    Fixed the failing test case to test print Fizz for case divide by 3

--- src/main/java/com/psl/fizzbuzz/FizzBuzz.java
+++ src/main/java/com/psl/fizzbuzz/FizzBuzz.java
@@ -5,8 +5,9 @@ public class FizzBuzz {
     public String printNumbers() {
         String number = "";
         for (int i=1; i<=100; i++) {
-            number = number + " " + i;
+            number = number + " " + (i%3==0? "Fizz" : i);
         }
+        System.out.println(number);
         return number;
     }
 }

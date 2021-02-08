82cb19ba05c93c26cbf1e4e02b8024293fc21f25
jairam_gauns <jairam_gauns@persistent.com>
2021-02-08 14:29:25 +0530

    Refactored code for case divide by 3

--- src/main/java/com/psl/fizzbuzz/FizzBuzz.java
+++ src/main/java/com/psl/fizzbuzz/FizzBuzz.java
@@ -5,9 +5,13 @@ public class FizzBuzz {
     public String printNumbers() {
         String number = "";
         for (int i=1; i<=100; i++) {
-            number = number + " " + (i%3==0? "Fizz" : i);
+            number = number + " " + (isFizz(i) ? "Fizz" : i);
         }
         System.out.println(number);
         return number;
+    }
+
+    private boolean isFizz(int i) {
+        return i%3==0;
     }
 }

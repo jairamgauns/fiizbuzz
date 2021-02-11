f3c1fa77009c02d133fa2ebcc833ce28f9ba357c
jairam_gauns <jairam_gauns@persistent.com>
2021-02-11 17:30:05 +0530

    FizzBuzz test cases and code added

--- src/main/java/com/psl/fizzbuzz/FizzBuzz.java
+++ src/main/java/com/psl/fizzbuzz/FizzBuzz.java
@@ -2,16 +2,24 @@ package com.psl.fizzbuzz;
 
 public class FizzBuzz {
 
-    public String printNumbers() {
-        String number = "";
+    public String buildOutput() {
+        StringBuilder result = new StringBuilder();
         for (int i=1; i<=100; i++) {
-            number = number + " " + (isFizz(i) ? "Fizz" : i);
+            result.append(computeValue(i)).append(" ");
         }
-        System.out.println(number);
-        return number;
+        System.out.println(result);
+        return result.toString();
     }
 
-    private boolean isFizz(int i) {
-        return i%3==0;
+    public String computeValue(int i) {
+        if(i%15 == 0) {
+            return "FizzBuzz";
+        } else if(i%3 == 0) {
+            return "Fizz";
+        }  else if(i%5 == 0) {
+            return "Buzz";
+        } else {
+            return String.valueOf(i);
+        }
     }
 }
--- src/test/java/com/psl/fizzbuzz/FizzBuzzTest.java
+++ src/test/java/com/psl/fizzbuzz/FizzBuzzTest.java
@@ -4,7 +4,6 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.util.Assert;
 
 @SpringBootTest
 public class FizzBuzzTest {
@@ -19,7 +18,54 @@ public class FizzBuzzTest {
 
     @Test
     public void testCanReturnFizzIFDivideByThree () {
-         String value = fizzBuzz.printNumbers();
-        Assertions.assertTrue(value.contains("Fizz"));
+        for(int i=3; i<100;i=i+3) {
+            if (i % 3 != 0 && i % 5 != 0) {
+                String value = fizzBuzz.computeValue(i);
+                Assertions.assertEquals("Fizz", value);
+                Assertions.assertNotEquals("Buzz", value);
+            }
+        }
+    }
+
+    @Test
+    public void testCanReturnFizzIFDivideByFive () {
+        for(int i=5; i<100;i=i+5) {
+            if(i%3!=0 && i%5!=0) {
+                String value = fizzBuzz.computeValue(i);
+                Assertions.assertEquals("Buzz", value);
+                Assertions.assertNotEquals("Fizz", value);
+            }
+        }
+    }
+
+    @Test
+    public void testCanReturnFizzBuzzIFDivideByFiveAndThree () {
+        for(int i=5; i<100;i=i+5) {
+            if(i%3!=0 && i%5!=0) {
+                String value = fizzBuzz.computeValue(i);
+                Assertions.assertEquals("FizzBuzz", value);
+            }
+        }
+    }
+
+    @Test
+    public void testReturnNumber () {
+        for(int i=1; i<100;i++) {
+            if(i%3!=0 && i%5!=0 && i%15!=0) {
+                String value = fizzBuzz.computeValue(i);
+                Assertions.assertEquals(String.valueOf(i), value);
+            }
+        }
+    }
+
+    @Test
+    public void testReturnOutputString () {
+        String output = fizzBuzz.buildOutput();
+        String[] result = output.split(" ");
+        for (int i = 1; i < 100; i++) {
+            if(i%3!=0 && i%5!=0 && i%15!=0) {
+                Assertions.assertEquals(String.valueOf(i), result[i - 1]);
+            }
+        }
     }
 }
